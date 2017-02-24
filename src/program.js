de891d8969a2f8ac5c7fda2f2e5f2b69d176c34a
FloFloo17 <flo.erwan@gmail.com>
2017-02-24 10:42:23 +0100

    Exercice 4

--- src/program.js
+++ src/program.js
@@ -1,4 +1,5 @@
 let chalk = require('chalk');
+var _ = require('lodash');
 
 let checkpointsService = require('./staticCheckpoints');
 
@@ -42,11 +43,15 @@ let transformCheckpoint = (checkpoint) => {
 
 let showCheckpoint = (checkpoint, index) => {
   console.log(chalk.green('CHECKPOINT'), chalk.yellow(index + 1));
-  for (var property in checkpoint) {
-    if (checkpoint.hasOwnProperty(property)) {
-      console.log(chalk.cyan(property.toUpperCase()), checkpoint[property]);
-    }
-  }
+
+  _.mapKeys(checkpoint, checkpointProporty => {
+    console.log(chalk.cyan(checkpointProporty));
+  });
+  // for (var property in checkpoint) {
+  //   if (checkpoint.hasOwnProperty(property)) {
+  //     console.log(chalk.cyan(property.toUpperCase()), checkpoint[property]);
+  //   }
+  // }
   console.log('\n');
 };
 
